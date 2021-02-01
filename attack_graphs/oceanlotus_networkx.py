f5a090fbf35baa71b228279c0f825cd82e8dae59
LI Zhenyuan <65760086@qq.com>
2021-02-01 09:51:49 +0800

    dll hijacking example

--- attack_graphs/oceanlotus_networkx.py
+++ attack_graphs/oceanlotus_networkx.py
@@ -9,6 +9,8 @@ def draw_oceanlotus():
 def draw_dll_hijacking():
     G = nx.DiGraph()
 
-    G.add_edge("attacker", "(c:\\windows\\(system32|syswow64)|{execution_path})\\*.dll", action="write", directly="no")
+    G.add_node("attacker", type="subject_user", entity="no", necessary="yes")
+    G.add_node("(c:\\windows\\(system32|syswow64)|{execution_path})\\*.dll", type="object_file", entity="yes", necessary="yes")
+    G.add_edge("attacker", "(c:\\windows\\(system32|syswow64)|{execution_path})\\*.dll", action="write", directly="no", necessary="yes")
 
     return G
