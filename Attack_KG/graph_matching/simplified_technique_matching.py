94ba618f096946d20991f2c7dc891189956dad79
LI Zhenyuan <li_zhenyuan@qq.com>
2021-05-28 19:15:59 +0800

    Create simplified_technique_matching.py

---
+++ Attack_KG/graph_matching/simplified_technique_matching.py
@@ -0,0 +1,18 @@
+# Simplified technique matching
+# Attack graph are stored in NetworkX instance
+
+AttacKG_node_types = ["Attacker", "ExeFile", "DocFile", "Registry", "Service", "Vulnerability", "NetLoc"]
+
+AttacKG_node_details = {}
+
+AttacKG_node_equivalence_groups =[
+
+]
+
+# Return a score range from 0 to 10
+def AttacKG_node_matching(node_a, node_b):
+    if
+    pass
+
+def AttacKG_matching():
+    pass
