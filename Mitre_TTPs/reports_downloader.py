2eefed53d612c870a4b507fdcbc0cd9e21abe622
LI Zhenyuan <li_zhenyuan@qq.com>
2021-05-28 19:15:27 +0800

    Update reports_downloader.py

--- Mitre_TTPs/reports_downloader.py
+++ Mitre_TTPs/reports_downloader.py
@@ -1,7 +1,7 @@
-from urllib.request import urlretrieve
+from urllib.request import urlretrieve, urlopen, Request
 
-
-def download_from_url(url, location):
-    print(url)
-    # urlretrieve(url, location)
-
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+url = 'https://www.itworld.com/article/2853992/3-tools-to-check-your-hard-drives-health-and-make-sure-its-not-already-dying-on-you.html'
+req = Request(url=url, headers=headers)
+html = urlopen(req).read()
+print(html)
