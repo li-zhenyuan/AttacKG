1cac8efd9744c858d18cd3a141bdcd6041954b09
LI Zhenyuan <li_zhenyuan@qq.com>
2021-06-08 15:30:44 +0800

    Update ner_with_spacy.py

--- NLP/ner_with_spacy.py
+++ NLP/ner_with_spacy.py
@@ -11,40 +11,10 @@ from spacy.pipeline import EntityRuler
 from spacy.lang.en import English
 import random
 import json
+import sys
 
-
-#%%
-
-def read_labeled_data(path: str = r"/home/zhenyuan/AttacKG/NLP/Doccano/2021-6-1.jsonl") -> list:
-    # labeled_data_path = r"/home/zhenyuan/AttacKG/NLP/Doccano/admin.jsonl"
-    labeled_data_path = path
-    labeled_data = []
-    with open(labeled_data_path, "r") as read_file:
-        for line in read_file:
-            data = json.loads(line)
-            labeled_data.append(data)
-
-    print('---Read Labeled Data(%d)!---' % len(labeled_data))
-    return labeled_data
-
-
-# split training and testing set
-# training_set = spacy_data[0:19]
-# testing_set = spacy_data[20:-1]
-
-def convert_data_format(labeled_data: list) -> list:
-    # Data format converting
-    spacy_data = []
-    for entry in labeled_data:
-        entities = []
-        for e in entry['label']:
-            entities.append((e[0], e[1], e[2]))
-        try:
-            nlp = spacy.load('en')
-            spacy_data.append(Example.from_dict(nlp.make_doc(entry['data']), {"entities": entities}))
-        except:
-            print("Wrong format: %s!" % entry['data'])
-    return spacy_data
+sys.path.append("..")
+from Attack_KG import AttacKG
 
 
 #%%
@@ -56,18 +26,15 @@ class NER_With_Spacy:
     model_location = None
     # model_location = "/home/zhenyuan/AttacKG/NLP/cti.model"
 
-    # ner_labels = ["NetLoc", "APTFamily", "ExeFile", "ScriptsFile", "DocumentFile", "E-mail", "Registry", "File", "Vulnerability", "C2C", "SensInfo", "Service"]
-    ner_labels = ["FilePath", "NetLoc", "FileName", "Vulnerability", "Registry", "Attacker", "ExeFile", "DocFIle",
-                  "Service"]
+    ner_labels = ["NetLoc", "APTFamily", "ExeFile", "ScriptsFile", "DocumentFile", "E-mail", "Registry", "File", "Vulnerability", "C2C", "SensInfo", "Service"]
+    # ner_labels = ["FilePath", "NetLoc", "FileName", "Vulnerability", "Registry", "Attacker", "ExeFile", "DocFIle","Service"]
 
     nlp = None
     optimizer = None
 
     def __init__(self, model_location=None):
         self.model_location = model_location
-        self.create_optimizer()
 
-    def create_optimizer(self):
         if self.model_location is None:
             self.nlp = spacy.blank('en')
             print("---Created Blank 'en' Model!---")
@@ -75,6 +42,34 @@ class NER_With_Spacy:
             self.nlp = spacy.load(self.model_location)
             print("---Load Model: %s!---" % self.model_location)
 
+        self.create_optimizer()
+
+    def read_labeled_data(self, path: str) -> list:
+        # labeled_data_path = r"/home/zhenyuan/AttacKG/NLP/Doccano/admin.jsonl"
+        labeled_data_path = path
+        labeled_data = []
+        with open(labeled_data_path, "r") as read_file:
+            for line in read_file:
+                data = json.loads(line)
+                labeled_data.append(data)
+
+        print('---Read Labeled Data(%d)!---' % len(labeled_data))
+        return labeled_data
+
+    def convert_data_format(self, labeled_data: list) -> list:
+        # Data format converting
+        spacy_data = []
+        for entry in labeled_data:
+            entities = []
+            for e in entry['label']:
+                entities.append((e[0], e[1], e[2]))
+            try:
+                spacy_data.append(Example.from_dict(self.nlp.make_doc(entry['data']), {"entities": entities}))
+            except:
+                print("Wrong format: %s!" % entry['data'])
+        return spacy_data
+
+    def create_optimizer(self):
         if 'ner' not in self.nlp.pipe_names:
             ner = self.nlp.add_pipe("ner")
         else:
@@ -118,17 +113,12 @@ class NER_With_Spacy:
         displacy.render(doc, style='ent')
 
     # https://stackoverflow.com/questions/57667710/using-regex-for-phrase-pattern-in-entityruler
+    # https://python.plainenglish.io/a-closer-look-at-entityruler-in-spacy-rule-based-matching-44d01c43fb6
     patterns = [
-        {"label": "Attacker", "pattern":
-            [{"TEXT": {"REGEX": "APT[A-Za-z0-9-]+"}}
-             ]},
-        {"label": "ExeFile", "pattern":
-            [{"TEXT": {"REGEX": "payload[s]"}},
-             {"TEXT": {"REGEX": "script[s]*"}}
-             ]},
-        {"label": "NetLoc", "pattern":
-            [{"TEXT": {"REGEX": "(E|e)[-]*mail[s]*"}}
-             ]}
+        {"label": "Attacker", "pattern": [{"TEXT": {"REGEX": "APT[A-Za-z0-9-]+"}}]},
+        {"label": "ExeFile", "pattern": [{"TEXT": {"REGEX": "payload[s]*"}}]},
+        {"label": "ExeFile", "pattern": [{"TEXT": {"REGEX": "script[s]*"}}]},
+        {"label": "NetLoc", "pattern": [{"TEXT": {"REGEX": "(E|e)[-]*mail[s]*"}}]}
     ]
 
     config = {
@@ -139,31 +129,37 @@ class NER_With_Spacy:
     }
 
     def ner_with_regex(self):
-        ruler = self.nlp.add_pipe("entity_ruler", config=self.config)
+        ruler = self.nlp.add_pipe("entity_ruler", config=self.config, before="ner")
         ruler.add_patterns(self.patterns)
 
-        doc = self.nlp("APT3 has used PowerShell on victim systems to download and run payloads after exploitation.")
-        print([(ent.text, ent.label_) for ent in doc.ents])
+        # doc = self.nlp("APT3 has used PowerShell on victim systems to download and run payloads after exploitation.")
+        # print([(ent.text, ent.label_) for ent in doc.ents])
 
 
 #%%
 
 if __name__ == '__main__':
-    ner_model = NER_With_Spacy()
-    labeled_data = read_labeled_data(r"C:\Users\xiaowan\Documents\GitHub\AttacKG\NLP\Doccano\admin.jsonl")
-    spacy_data = convert_data_format(labeled_data)
+    sample = "APT3 has used PowerShell on victim systems to download and run payloads after exploitation."
+
+    ner_model = NER_With_Spacy("en_core_web_sm")
+    # ner_model = NER_With_Spacy()
+    labeled_data = ner_model.read_labeled_data(r"C:\Users\xiaowan\Documents\GitHub\AttacKG\NLP\Doccano\admin.jsonl")
+    spacy_data = ner_model.convert_data_format(labeled_data)
     ner_model.train_model(spacy_data)
+
+    # ner_model.nlp.replace_pipe("parser", "parser", source="en_core_web_sm")
+    doc = ner_model.nlp(sample)
+    print([(ent.text, ent.label_) for ent in doc.ents])
+
+    ner_model.ner_with_regex()
+    doc = ner_model.nlp(sample)
+    print([(ent.text, ent.label_) for ent in doc.ents])
+    sents = [sent for sent in doc.sents]
     pass
+
 
 #%%
 
-sample = "APT3 has used PowerShell on victim systems to download and run payloads after exploitation."
-
-# nlp = spacy.load("./new_cti.model")
-# doc = ner_model.nlp(sample)
-
-ner_model.ner_with_regex()
-doc = ner_model.nlp(sample)
 
 
 #%%
