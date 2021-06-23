48b81713dc9a1a9558668a12f60952ecffbcc893
LI Zhenyuan <li_zhenyuan@qq.com>
2021-06-23 15:14:19 +0800

    tree-based template

--- Attack_KG/template_extraction.py
+++ Attack_KG/template_extraction.py
@@ -34,20 +34,94 @@ class Template_node:
     details = {}
 
     follow_up_count = 0
-    possible_follow_up = [] # (leaf_node, count)
+    possible_follow_up = [] # [leaf_node, count]
+
+    def __init__(self, type = "APTFamily"):
+        self.type = type
+        self.follow_up_count = 0
+        self.possible_follow_up = []
+
+    def __str__(self, level=0):
+        ret = "\t"*level + self.type + ":" + str(self.follow_up_count) + "\n"
+        for child in self.possible_follow_up:
+            ret += child[0].__str__(level+1)
+        return ret
+
+    def add_follow_up(self, single_tree):
+        self.follow_up_count += 1
+        print("--Add {} to {}".format(single_tree.type, self.type))
+
+
+        for i in range(0, len(self.possible_follow_up)):
+            f, count = self.possible_follow_up[i]
+            if node_similarity(single_tree, f) >= 0.5:
+                self.possible_follow_up[i][1] += 1
+                # ToDo: update leaf details
+
+                if single_tree.follow_up_count != 0:
+                    self.possible_follow_up[i][0].add_follow_up(single_tree.possible_follow_up[0][0])
+
+                return
+
+        self.possible_follow_up.append([single_tree, 1])
+
+
+def node_similarity(n: Template_node, m: Template_node):
+    # ToDo: take details into consideration
+    similarity = 0
+
+    if n.type == m.type:
+        similarity += 0.5
+
+    return similarity
 
 # %%
 
+def attack_graph_to_sequence(G):
+    template_node_list = []
+
+    for node in G.nodes:
+        # print(G.nodes[node]["type"])
+
+        if G.nodes[node]["type"] == "APTFamily":
+            continue
+
+        node = Template_node(G.nodes[node]["type"])
+        template_node_list.append(node)
+
+    return template_node_list
+
+def node_list_to_singletree(node_list):
+    # node_list = attack_graph_to_sequence(G)
+    print("---Generate singletree!---")
+
+    n = node_list[-1]
+    for i in range(len(node_list)-2, -1, -1):
+        node_list[i].add_follow_up(n)
+        n = node_list[i]
+
+    for node in node_list:
+        print(node.type + ":" + str(len(node.possible_follow_up)))
+
+    return n
+
+
 class Technique_template:
 
-    node_set = []
-    edge_set = []
-
-    variant_list = []
+    root_node = Template_node()
 
     def update_template(self, G):
-        pass
 
+        node_list = attack_graph_to_sequence(G)  # ToDo: Sort the node_list before build template tree
+        if len(node_list) == 0:
+            return
+        single_tree = node_list_to_singletree(node_list)
+        print(single_tree)
+
+        print("---Update template!---")
+        self.root_node.add_follow_up(single_tree)
+
+        return single_tree
 
 
 # %%
@@ -55,14 +129,33 @@ class Technique_template:
 if __name__ == '__main__':
     technique_example_dict = get_technique_example_dict()
 
+#%%
+
     # '/techniques/T1566/00[1|2|3]',
-    phishing_email_example = technique_example_dict[r'/techniques/T1566/001']
+    phishing_email_example = []
+
+    # phishing_email_example += technique_example_dict[r'/techniques/T1566/001']
+    # phishing_email_example += technique_example_dict[r'/techniques/T1566/002']
+    # phishing_email_example += technique_example_dict[r'/techniques/T1566/003']
+
+    phishing_email_example += technique_example_dict[r'/techniques/T1114/001']
+    phishing_email_example += technique_example_dict[r'/techniques/T1114/002']
+
+#%%
 
     ag = AttacKG_AG()
     ner_model = NER_With_Spacy("./new_cti.model")
     # ner_model = NER_With_Spacy("en_core_web_sm")
 
+    tt = Technique_template()
+
     for example in phishing_email_example:
+        print(example)
+
         doc = ner_model.parser(example)
         G = ag.construct_AG_from_spacydoc(doc)
-        view_graph(G)
+        # view_graph(G)
+
+        # node_list = tt.update_template(G)
+        single_tree = tt.update_template(G)
+        print(tt.root_node)
