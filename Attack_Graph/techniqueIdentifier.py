d07eab85ca2d8bd7374823e907a133f44d2d97f4
LI Zhenyuan <li_zhenyuan@qq.com>
2021-07-30 20:20:48 +0800

    Create techniqueIdentifier.py

---
+++ Attack_Graph/techniqueIdentifier.py
@@ -0,0 +1,14 @@
+from attackGraph import *
+from attackTemplate import *
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import logging
+import os.path
+
+
+class TechniqueIdentifier:
+    technique_template_list: list
+
+    def node_alignment:
+        
