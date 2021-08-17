dd03c4b88adff055bfb14af5ee1da5c2fdddc50c
LI Zhenyuan <li_zhenyuan@qq.com>
2021-08-17 11:35:11 +0800

    Update techniqueGraphBuilder.py

--- Attack_Graph/techniqueGraphBuilder.py
+++ Attack_Graph/techniqueGraphBuilder.py
@@ -9,6 +9,21 @@ class TechniqueGraphBuilder:
     attack_graph: AttackGraph
     attack_match: AttackMatcher
 
-    def __init__(self, attack_graph: AttackGraph, attack_match: AttackMatcher):
-        
+    technique_graph_nx: nx.DiGraph
+    technique_subgraph_dict: dict  # technique -> cluster
 
+    def __init__(self, attack_graph: AttackGraph, attack_match: AttackMatcher):
+        self.attack_graph = attack_graph
+        self.attack_match = attack_match
+
+        self.technique_graph_nx = self.attack_graph.attackgraph_nx
+        self.technique_subgraph_dict = {}
+
+    def identify_technique_cluster(self):
+        pass
+
+
+# %%
+
+if __name__ == '__main__':
+    pass
