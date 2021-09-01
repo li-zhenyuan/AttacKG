fb334089add0bd88dd6134398f02aa04b2b5d7b4
LI Zhenyuan <li_zhenyuan@qq.com>
2021-09-01 10:21:09 +0800

    2021-9-1

--- NLP/keywordExtraction.py
+++ NLP/keywordExtraction.py
@@ -3,16 +3,31 @@ import re
 
 # %%
 
-if __name__ == '__main__':
-    example_file = "produce_examples_picked.txt"
-    output_file = "produce_examples_picked_single_keywords.txt"
+example_file = "produce_examples_picked.txt"
+output_file = "produce_examples_picked_single_keywords.txt"
 
-    with open(example_file, 'r') as example_input, open(output_file, 'w+') as output:
-        example_list = example_input.readlines()
+keyword_list = []
 
-        kw_model = KeyBERT()
-        for example in example_list:
-            example = re.sub("\[[0-9]+\]+", "", example)
-            keywords = kw_model.extract_keywords(example, keyphrase_ngram_range=(1,1))
-            print(keywords)
-            output.write(str(keywords) + "\n")
+with open(example_file, 'r') as example_input, open(output_file, 'w+') as output:
+    example_list = example_input.readlines()
+
+    kw_model = KeyBERT()
+    for example in example_list:
+        example = re.sub("\[[0-9]+\]+", "", example)
+        keywords = kw_model.extract_keywords(example, keyphrase_ngram_range=(1,1))
+        print(keywords)
+        keyword_list += keywords
+        output.write(str(keywords) + "\n")
+
+
+# %%
+
+keywords_count_dict = {}
+
+for keyword in keyword_list:
+    keyword_string = keyword[0]
+
+    if keyword_string not in keywords_count_dict.keys():
+        keywords_count_dict[keyword_string] = 1
+    else:
+        keywords_count_dict[keyword_string] += 1
